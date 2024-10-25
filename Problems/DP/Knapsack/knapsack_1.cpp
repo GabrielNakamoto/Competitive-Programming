8fdd4279a9ea04e543e5da7b643c8f1547fb818f
Gabriel Nakamoto <gabriel@nakamoto.ca>
2024-10-25 18:36:20 -0400

    Completed first knapsack problem

--- Problems/DP/Knapsack/knapsack_1.cpp
+++ Problems/DP/Knapsack/knapsack_1.cpp
@@ -11,11 +11,35 @@
 
 using namespace std;
 
+const int inf=2e9;
+const int mxN=100+10;
+const int mxW=1e5+10;
+
+long long dp[mxW][mxN];
+array<int, 2> a[mxN];
+
 int main(){
     ios_base::sync_with_stdio(0); cin.tie(0);
 
-    int n, w;
-    cin>>n>>w;
+    int n, W;
+    cin>>n>>W;
+
+    for(int i=1; i<=n; ++i)
+        cin>>a[i][0]>>a[i][1];
+
+    for(int i=0; i<W; ++i)
+        dp[i][0]=0;
+    for(int i=0; i<W; ++i)
+        for(int j=1; j<=n; ++j)
+            dp[i][j]=-inf;
+
+    for(int i=1; i<=n; ++i){
+        for(int w=0; w<=W; ++w){
+            if(w - a[i][0]>=0) dp[w][i]=dp[w-a[i][0]][i-1] + a[i][1];
+            dp[w][i]=max(dp[w][i],dp[w][i-1]);
+        }
+    }
+    cout<<dp[W][n];
 
     return 0;
 }
